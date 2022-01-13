860e1a6292f19dca180b9d829470adb8872a4330
kodirov8788 <kodirov8788@gmail.com>
2022-01-13 11:57:32 +0500

    lskdl;skad

--- index.js
+++ index.js
@@ -8,46 +8,46 @@ const io = new Server({
 
 
 
-// let onlineUsers = [];
+let onlineUsers = [];
 
-// const addNewUser = (username, socketId) => {
-//   !onlineUsers.some((user) => user.username === username) &&
-//     onlineUsers.push({ username, socketId });
-// };
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
 
-// const removeUser = (socketId) => {
-//   onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-// };
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
 
-// const getUser = (username) => {
-//   return onlineUsers.find((user) => user.username === username);
-// };
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
 
 io.on("connection", (socket) => {
   console.log("connected >>>");
 
-  // socket.on("newUser", (username) => {
-  //   addNewUser(username, socket.id);
-  // });
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
+  });
 
   io.emit("server", "Hello world again");
-  // socket.on("sendNotification", ({ senderName, receiverName, type }) => {
-  //   const receiver = getUser(receiverName);
-  //   io.to(receiver.socketId).emit("getNotification", {
-  //     senderName,
-  //     type,
-  //   });
-  // });
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      type,
+    });
+  });
 
-  // socket.on("sendText", ({ senderName, receiverName, text }) => {
-  //   const receiver = getUser(receiverName);
-  //   io.to(receiver.socketId).emit("getText", {
-  //     senderName,
-  //     text,
-  //   });
-  // });
+  socket.on("sendText", ({ senderName, receiverName, text }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getText", {
+      senderName,
+      text,
+    });
+  });
   socket.on("disconnect", () => {
-    // removeUser(socket.id);
+    removeUser(socket.id);
     console.log("disconnected >>>");
   });
 });
